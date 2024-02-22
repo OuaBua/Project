a442e5fa0868e3b7d58e1f36a6fc0850c0abfdbe
ouabua <ouabua@naver.com>
2024-02-22 20:24:28 +0900

    03. GItFinder 만들기

---
+++ GitFinder/github.js
@@ -0,0 +1,36 @@
+class Github {
+    constructor() {
+      this.client_id = 'e932ba1d163e2d200af4';
+      this.client_secret = 'ae9f3a7ec4b8dbdf6ab4108dd7af75030dfbb';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
+    }
+  
+    async getUser(user) {
+      const profileResponse =
+        await fetch(
+          `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+  
+      const eventsResponse =
+         await fetch(
+          `https://api.github.com/users/${user}/events/public?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      );
+          
+
+      const repoResponse =
+        await fetch(
+          `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+  
+      const profile = await profileResponse.json();
+      const events = await eventsResponse.json();
+      const repos = await repoResponse.json();
+  
+      return {
+        profile,
+        events,
+        repos
+      }
+    }
+  }
